a6652126d10fd5dccd7435d4d4ae8a7f33bf7f88
Brandon Suckerman <brandonsuckie@gmail.com>
2025-10-21 15:06:04 -0400

    frr
    frrfr

--- src/App.java
+++ src/App.java
@@ -11,12 +11,17 @@ public class App extends PApplet {
   float speed = 2;
   float xstart = 60;
   float ystart = 440;
-  int deaths = 0;
+  int deaths, prevDeaths = 0;
   int wins = 0;
   int time = 0;
   boolean left, up, down, right;
+  boolean won = false;
+  boolean startScreen = true;
+  PImage ballImage;
+  
 
   public void setup() {
+    ballImage = loadImage("brandon.png");
   }
 
   public void settings() {
@@ -24,13 +29,24 @@ public class App extends PApplet {
   }
 
   public void draw() {
-
     background(0);
-    fill(0, 0, 255);
-    circle(x, y, size);
+    if (startScreen) {
+      fill(255);
+      textSize(80);
+      text("Maze Game", 70, 100);
+      textSize(25);
+      text("Press 1 to play easy", 90, 150);
+      textSize(25);
+      text("Press 2 to play hard", 90, 180);
+      textSize(18);
+      text("Use arrow keys to move",90, 220 );
+      return; 
+    }
+    time += 1;
+    background(0);
 
     fill(255, 0, 0);
-
+    noStroke();
     rect(0, 0, 500, 40);
     rect(0, 0, 40, 500);
     rect(0, 460, 500, 40);
@@ -42,42 +58,46 @@ public class App extends PApplet {
     rect(290, 290, 120, 500);
     rect(360, 80, 60, 500);
     fill(0, 255, 0);
-    rect(420, 450, 40, 500);
+    rect(420, 450, 40, 550);
+    
+    fill(0);
+    rect(15, 400, 100, 70);
+
+    imageMode(CENTER);
+    image(ballImage, x, y, size, size);
+    
+
     fill(255, 255, 255);
     textSize(45);
-    text("wins:" + wins, 200, 450, 100);
+    text("wins:" + wins, 200, 400, 100);
+    
+    if (!won) {
+      if (left == true) {
+        x -= speed;
+      }
+      if (up == true) {
+        y -= speed;
+      }
+      if (down == true) {
+        y += speed;
+      }
+      if (right == true) {
+        x += speed;
+      }
+    }
+    if (won) {
 
-    if (left == true) {
-      x -= speed;
-    }
-    if (up == true) {
-      y -= speed;
-    }
-    if (down == true) {
-      y += speed;
-    }
-    if (right == true) {
-      x += speed;
+      fill(255, 255, 255);
+      textSize(45);
+      text("You won after " + prevDeaths + " deaths!", 25, 250);
+
+      if (time % 300 == 0) {
+        won = false;
+
+      }
     }
 
     int currentColor = get((int) x, (int) y);
-
-    currentColor = get((int) x, (int) (y + size / 2));
-    if (green(currentColor) == 255 && red(currentColor) == 0 && blue(currentColor) == 0 && time == 0) {
-      time = millis();
-      wins++;
-
-    }
-    if (time > 0) {
-      fill(255, 255, 255);
-      textSize(45);
-      text("You won after " + deaths + " deaths!", 50, 250);
-    }
-    if (millis()-time > 1000) {
-      x = xstart;
-      y = ystart;
-      time=0;
-    }
 
     currentColor = get((int) x, (int) (y - size / 2));
     if (red(currentColor) == 255 && green(currentColor) == 0 && blue(currentColor) == 0) {
@@ -110,13 +130,22 @@ public class App extends PApplet {
       deaths++;
 
     }
+    currentColor = get((int) x, (int) (y + size / 2));
+    if (green(currentColor) == 255 && red(currentColor) == 0 && blue(currentColor) == 0) {
+      wins++;
+      won = true;
+      x = xstart;
+      y = ystart;
+      prevDeaths = deaths;
+      deaths = 0;
+    }
   }
+
   // used chat gpt for only the part where when the ball touches
   // a specific color it does somthing, the green makes a text and
-  // red makes ball go back to begining, but did the rest myself
+  // red makes ball go back to begining.
 
-
-  
+ 
 
   public void keyPressed() {
     if (keyCode == UP) {
@@ -127,6 +156,12 @@ public class App extends PApplet {
       left = true;
     } else if (keyCode == RIGHT) {
       right = true;
+    }
+       if (startScreen && key == '1') {
+      startScreen = false;
+      x = xstart;
+      y = ystart;
+      return;
     }
 
   }
